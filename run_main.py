e027aebc5d163fe2ab33e5a913cce11e8e815409
f1anatriello <anatriello.1957447@studenti.uniroma1.it>
2025-12-17 16:42:06 +0100

    added compiled bytecode for PRNG classes

--- run_main.py
+++ run_main.py
@@ -6,9 +6,12 @@ from visual_test import visualize_random_dots
 from visual_test import visualize_wave  
 
 def main():
-    prng = SeedlessLCG()
-    prng.refresh(os.urandom(8))
-    visualize_random_dots(prng, width=550, height=600)
+    prng1 = SeedlessLCG()
+    prng2 = SeedlessRC4()
+    prng3 = SeedlessXorshift()
+    prng1.refresh(os.urandom(8))
+    prng1.refresh(os.urandom(16))
 
+    visualize_wave(prng1, prng2, width=600, step=20, amplitude=50, baseline1=100, baseline2=300)
 if __name__ == "__main__":
     main()
